1ad0567b7e0595222d155ba525432ee71747302d
Dante Gates <dgates@O2O-PA-DGATES.fios-router.home>
2019-02-07 09:01:08 -0500

    updated functions to use set of removed indices

--- bpe_utils.pyx
+++ bpe_utils.pyx
@@ -1,21 +1,24 @@
-def get_stats(list vocab):
-    cdef int i
+def get_stats(list vocab, set removed_indices):
+    cdef int i_left, i_right
     cdef dict pairs = {}
     cdef dict indices = {}
-    for i in range(len(vocab) - 1):
-        pair = vocab[i], vocab[i+1]
+    valid_indices = (i for i in range(len(vocab) - 1)
+                     if not i in removed_indices)
+    i_left = next(valid_indices)
+    for i_right in valid_indices:
+        pair = vocab[i_left], vocab[i_right]
         if not pair in pairs:
             pairs[pair] = 0
         pairs[pair] += 1
         if not pair in indices:
             indices[pair] = []
-        indices[pair].append(i)
+        indices[pair].append(i_left)
     return pairs, indices
 
-def merge_vocab(tuple pair, list vocab, list indices):
+def merge_vocab(tuple pair, list vocab, list pair_indices, set removed_indices):
     cdef str new = ''.join(pair)
     cdef int i
-    for i in reversed(indices):
+    for i in pair_indices:
         vocab[i] = new
-        vocab.pop(i+1)
+    removed_indices.update(pair_indices)
     return vocab
